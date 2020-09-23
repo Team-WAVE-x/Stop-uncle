948c92032359099b7e14e6e2fbcf33978043493d
시혁 박 <exon.indiv@gmail.com>
2020-09-23 19:25:24 +0900

    example

---
+++ example.js
@@ -0,0 +1,12 @@
+const axios = require('axios')
+
+axios.request({
+  method: 'GET',
+  url: 'http://localhost:8080/',
+  headers: { 'Content-Type': 'application/json' },
+  params: { quiz: 'random' }
+}).then(res => {
+  const { data: responseBody, status: responseCode } = res
+  console.log(responseBody)
+  console.log(responseCode)
+})
