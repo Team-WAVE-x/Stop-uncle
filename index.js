dea8d4d3a8971dc5e5570dc4c33c1cc048aa3f15
시혁 박 <exon.indiv@gmail.com>
2020-09-23 16:55:27 +0900

    init

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const http = require('http');
 const ajegag = require('./ajegag.json')
-let url = require('url');
-let querystring = require('querystring'); 
+const url = require('url');
+const querystring = require('querystring'); 
 
-let server = http.createServer(function(request,response){
-
+let server = http.createServer((request,response) => {
     let parsedUrl = url.parse(request.url);
 
     let parsedQuery = querystring.parse(parsedUrl.query,'&','=');
 
     let quiz = ajegag.problems[Number(parsedQuery.quiz)]
-    console.log(d);
+    console.log(quiz);
 
     response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-    response.end(JSON.stringify(d));
+    response.end(JSON.stringify(quiz));
 });
 
 server.listen(8080, function(){
