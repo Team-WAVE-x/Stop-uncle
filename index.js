d4f77c346826188a0afd65e1a9a5a6d359ecb4dd
시혁 박 <exon.indiv@gmail.com>
2020-09-23 18:21:28 +0900

    add random

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const ajegag = require('./ajegag.json')
 const app = express()
 app.use((req, res) => {
   const { quiz } = req.query
-  res.send(ajegag.problems[quiz])
+  if (quiz === 'random') res.send(ajegag.problems[Math.floor(Math.random() * 43)])
+  else res.send(ajegag.problems[quiz])
 })
 
 app.listen(8080, () => console.log('Server is running...'))
