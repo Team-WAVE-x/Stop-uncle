73ace6474510ed09264da37f541e6eab369a3470
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2020-09-23 17:28:38 +0900

    Minimalism

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const http = require('http');
+const express = require('express')
 const ajegag = require('./ajegag.json')
-const url = require('url');
-const querystring = require('querystring'); 
 
-let server = http.createServer((request,response) => {
-    let parsedUrl = url.parse(request.url);
+const app = express()
+app.use((req, res) => {
+  const { quiz } = req.query
+  res.send(ajegag.problems[quiz])
+})
 
-    let parsedQuery = querystring.parse(parsedUrl.query,'&','=');
-
-    let quiz = ajegag.problems[Number(parsedQuery.quiz)]
-    console.log(quiz);
-
-    response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-    response.end(JSON.stringify(quiz));
-});
-
-server.listen(8080, function(){
-    console.log('Server is running...');
-});
+app.listen(8080, () => console.log('Server is running...'))
