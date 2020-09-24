2b08bb734613af5c2cde3a813c13fc1af54e38d9
시혁 박 <exon.indiv@gmail.com>
2020-09-24 18:37:24 +0900

    Add Error

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express()
 app.use((req, res) => {
   const { quiz } = req.query
   if (quiz === 'random') res.send(ajegag.problems[Math.floor(Math.random() * ajegag.problems.length)])
-  else res.send(ajegag.problems[quiz])
+  else if (ajegag.problems[quiz]) res.send(ajegag.problems[quiz])
+  else res.send('ERROR')
 })
 
 app.listen(8080, () => console.log('Server is running...'))
