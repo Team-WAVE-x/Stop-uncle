7012c76fb7d038d0705e88f82b98bdbca2641c0e
Rlato <park20050620@daum.net>
2020-10-05 23:37:17 +0900

    인증시스템 추가
    db는책임안짐 알아서 하삼

--- example.js
+++ example.js
@@ -3,7 +3,10 @@ const axios = require('axios')
 axios.request({
   method: 'GET',
   url: 'http://localhost:8080/',
-  headers: { 'Content-Type': 'application/json' },
+  headers: { 
+    'Content-Type': 'application/json',
+    'token' : 'F9e265G1efgrLfmaZ5Ut6ohjemal6D0-'
+  },
   params: { quiz: 'random' }
 }).then(res => {
   const { data: responseBody, status: responseCode } = res
