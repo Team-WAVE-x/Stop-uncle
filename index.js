d7f34f28fd1f4d5e7a42a90ab6cf75754755f6ee
EXON <exon.indiv@gmail.com>
2021-03-04 17:56:13 +0900

    Simplify: code

--- index.js
+++ index.js
@@ -11,23 +11,18 @@ const repeat = schedule.scheduleJob('* * 0 * *', function() {
 
 const app = express()
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(function (req, res) {
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use((req, res) => {
   const { quiz } = req.query
   const req_token = req.get('token')
   db.getTokenInfo(req_token).then((rows) => {
-    if(rows === undefined){
-      if (quiz === 'exonbabo') {
-        return res.send('와 엑슨바보 아시는구나').end()
+    if (rows === undefined) {
+      if (quiz === 'exonbabo') return res.send('와 엑슨바보 아시는구나').end()
+      else return res.status(401).send({ message: "Unknown token" }).end()
     } else {
-      return res.status(401).send({ message: "Unknown token" }).end()
-    }
-    }else{
-      if(rows.is_banned === 1){
-        return res.status(403).send({ message: "banned token" }).end()
-      }else if(rows.call_count >= max){
-        res.status(202).send({ message: "daily limit exceeded" }).end()
-      }else{
+      if (rows.is_banned) return res.status(403).send({ message: "banned token" }).end()
+      else if(rows.call_count >= max) res.status(202).send({ message: "daily limit exceeded" }).end()
+      else {
         db.updateTokenInfo(req_token, rows.call_count+1)
         if (quiz === 'random') res.send(ajegag.problems[Math.floor(Math.random() * ajegag.problems.length)])
         else if (ajegag.problems[quiz]) res.send(ajegag.problems[quiz]).end()
